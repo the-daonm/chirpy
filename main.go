e142a98f566568eb11a52e2bf0bb6aeb7c8d9f66
daonm <daobilionusd@gmail.com>
2026-04-26 15:43:18 +0700

    feat: add custom handler and change path / to /app

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Addr:  	 ":" + port,
@@ -20,4 +20,10 @@ func main() {
 
 	log.Printf("Serving file from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
